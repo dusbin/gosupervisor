3b5a5d155e993fba522c484336b69b6f3118bcbe
杜争斌 <duzhengbin@suanier.com>
2021-05-10 15:42:09 +0800

    read config

--- conf/parse.go
+++ conf/parse.go
@@ -1,7 +1,6 @@
 package conf
 
 import (
-	"fmt"
 	"github.com/Unknwon/goconfig"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -27,9 +26,7 @@ func Parse(t readType,data string) error {
 	if err != nil {
 		return errors.WithMessage(err,"Parse failed")
 	}
-	fmt.Printf("")
 	sectionList := cfg.GetSectionList()
-	fmt.Println("sectionList:",sectionList)
 	for _,section := range sectionList {
 		switch section{
 		case unixHttpServer :
@@ -57,40 +54,7 @@ func Parse(t readType,data string) error {
 				logrus.Warn("not support section:",section)
 			}
 		}
-		keyList := cfg.GetKeyList(section)
-		fmt.Println("keyList:",keyList)
 	}
-
-	userListKey, err := cfg.GetValue("","USER_LIST")
-	if err != nil {
-		return errors.Unwrap(err)
-	}
-	fmt.Println(userListKey)
-	userListKey2,_ := cfg.GetValue(goconfig.DEFAULT_SECTION, "USER_LIST")
-	fmt.Println(userListKey2)
-	maxCount := cfg.MustInt("","MAX_COUNT")
-	fmt.Println(maxCount)
-	maxPrice := cfg.MustFloat64("","MAX_PRICE")
-	fmt.Println(maxPrice)
-	isShow := cfg.MustBool("","IS_SHOW")
-	fmt.Println(isShow)
-
-	db := cfg.MustValue("test","dbdns")
-	fmt.Println(db)
-
-	dbProd := cfg.MustValue("prod","dbdns")
-	fmt.Println(dbProd)
-
-	//set 值
-	cfg.SetValue("","MAX_NEW","10")
-	maxNew := cfg.MustInt("","MAX_NEW")
-	fmt.Println(maxNew)
-
-	maxNew1,err := cfg.Int("","MAX_NEW")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(maxNew1)
 	return nil
 }
 
